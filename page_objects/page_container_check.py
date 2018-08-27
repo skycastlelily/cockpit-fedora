63d9f20a8728e85f492a9807c14af0c86f6520dc
skycastlelily <42671468+skycastlelily@users.noreply.github.com>
2018-08-27 21:43:08 +0800

    Update page_container_check.py

--- page_objects/page_container_check.py
+++ page_objects/page_container_check.py
@@ -10,12 +10,13 @@ class DockerCheckPage(SeleniumTest):
     :avocado: disable
     """
 
-    DOCKER_LINK = "#sidebar-menu a[href='/docker']"
+       DOCKER_LINK = "#sidebar-menu a[href='/docker']"
     DOCKER_FRAME_NAME = "/docker"
     #DOCKER_START_BUTTON = "//*[@class=blank-state-pf-main-action and data-action='docker-start']"
     DOCKER_START_BUTTON = "//*[@data-action='docker-start']"
-    #DOCKER_RUN_BUTTON = "//*[@class='btn btn-default btn-control-ct fa fa-play']"
-    DOCKER_RUN_BUTTON = "//button[contains(@class, 'fa-play')]"
+    WAIT_DOCKER_START=20
+    DOCKER_RUN_BUTTON = "div#containers-images>div>table>tbody>tr>td:nth-of-type(6)>button"
+    #DOCKER_RUN_BUTTON = "div#containers-images>div>table"
     DOCKER_RUN_COMMAND = "#containers-run-image-command"
     DOCKER_RUN_RUN = "#containers-run-image-run"
 
@@ -23,24 +24,33 @@ class DockerCheckPage(SeleniumTest):
         self.click(self.DOCKER_LINK)
         self.switch_to_frame(self.DOCKER_FRAME_NAME)
 
-    def start_container(self, username='lnie',passwd='testredhat'):
+    def start_container(self):
         self.hover_and_click(self.DOCKER_START_BUTTON)
         sleep(self.WAIT_DOCKER_START)
+    def run_container_cmd(self):
+        cmd = "docker run -it --name='extending' fedora /bin/bash &"
+        self.host.execute(cmd)
+        cmd = "docker run -it --name='flash' fedora ls"
+        self.host.execute(cmd)
 
-        #user_realname_input = "#accounts-create-real-name"
+    def stop_container_cmd(self):
+        cmd = 'docker stop extending '
+        self.host.execute(cmd)
 
-        #self.input_text(user_passwd2_input, passwd)
-        #self.hover_and_click(create_button)
-    def run_container(self,command='/bin/bash'):
+    def delete_container_cmd(self):
+        cmd = 'docker rm extending '
+        self.host.execute(cmd)
+
+    def run_container_ui(self,command='/bin/bash'):
         self.hover_and_click(self.DOCKER_RUN_BUTTON)
         sleep(2)
         self.input_text(DOCKER_RUN_COMMAND,command )
         self.hover_and_click(self.DOCKER_RUN_RUN)
         sleep(3)
+
     def get_container_status(self):
         container_run_status="tbody:nth-child(1) tr:nth-child(1) td:nth-child(6)"
         self.get_text(container_run_status)
-
 
 
 
