cb68ef5058d6d7aade7406de86c0738400d21d3d
skycastlelily <42671468+skycastlelily@users.noreply.github.com>
2018-08-27 21:41:29 +0800

    Update test_docker_check.py

--- test_suits/test_docker_check.py
+++ test_suits/test_docker_check.py
@@ -1,4 +1,5 @@
 import os
+from time import sleep
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 from page_objects.page_container_check import DockerCheckPage
@@ -9,12 +10,26 @@ class TestDockerCheck(DockerCheckPage):
     :avocado: enable
     :avocado: tags=docker
     """
+    SELECT_BUTTON = "div#containers-containers-filter>button"
+    SELECT_RUNNING_CONTAINER = "//li[@data-value='Images and running containers']"
+    SELECT_EVERYTHING = "//li[@data-value='Everything']"
     CONTAINER_FILTER = "#containers-filter"
-#    def test_startcontainer(self):
- #       self.start_container()
-  #      self.assert_element_visible(self.CONTAINER_FILTER)
+    container_running_text = "//td[text()='running']"
+    container_exited_text = "//td[text()='exited']"
+    def test_startcontainer(self):
+       # self.start_container()
+        self.assert_element_visible(self.CONTAINER_FILTER)
 
-    def test_runcontainer(self):
-        self.run_container()
-        container_status = self.get_container_status()
-        self.assertEqual(container_status,'running')
+    def test_cmdrun(self):
+        self.click(self.SELECT_BUTTON)
+        self.hover_and_click(self.SELECT_RUNNING_CONTAINER)
+        self.run_container_cmd()
+        self.assert_element_visible(self.container_running_text)
+        self.assert_element_invisible(self.container_exited_text)
+    def test_everything(self):
+        self.click(self.SELECT_BUTTON)
+        self.hover_and_click(self.SELECT_EVERYTHING)
+        sleep(3)
+        self.assert_element_visible(self.container_running_text)
+        self.assert_element_visible(self.container_exited_text)
+
