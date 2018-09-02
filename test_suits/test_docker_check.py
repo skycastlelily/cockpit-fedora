68675ac27eabfb06cbfe3e1fca00e919c3db2189
skycastlelily <42671468+skycastlelily@users.noreply.github.com>
2018-09-02 21:36:59 +0800

    Update test_docker_check.py

--- test_suits/test_docker_check.py
+++ test_suits/test_docker_check.py
@@ -15,10 +15,15 @@ class TestDockerCheck(DockerCheckPage):
     SELECT_EVERYTHING = "//li[@data-value='Everything']"
     CONTAINER_FILTER = "#containers-filter"
     container_running_text = "//td[text()='running']"
-    container_exited_text = "//td[text()='exited']"
+    container_exited_text = "//td[text()='exited']" 
+    image_pull_name = "//th[contains(text(), 'nginx')]"
     def test_startcontainer(self):
-       # self.start_container()
+        self.start_container()
         self.assert_element_visible(self.CONTAINER_FILTER)
+
+    def test_pull_image(self):
+        self.docker_pull_image()
+        self.assert_element_visible(self.image_pull_name)
 
     def test_cmdrun(self):
         self.click(self.SELECT_BUTTON)
@@ -26,10 +31,24 @@ class TestDockerCheck(DockerCheckPage):
         self.run_container_cmd()
         self.assert_element_visible(self.container_running_text)
         self.assert_element_invisible(self.container_exited_text)
+
     def test_everything(self):
         self.click(self.SELECT_BUTTON)
         self.hover_and_click(self.SELECT_EVERYTHING)
         sleep(3)
         self.assert_element_visible(self.container_running_text)
         self.assert_element_visible(self.container_exited_text)
+        def test_cmd_stop(self):
+        self.stop_container_cmd()
+        self.assert_element_invisible(self.container_running_text)
+
+    def test_cmd_delete(self):
+        running_container_name = "//th[text()='extending']"
+        self.delete_container_cmd()
+        self.assert_element_invisible(running_container_name)
+
+    def test_remove_image(self):
+        self.docker_rm_image()
+        self.assert_element_invisible(self.image_pull_name)
+
 
